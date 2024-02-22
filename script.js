7ab7a36bbbf2a9987251bde979309697af7ad357
tholkappiar <tholkappiar2003@gmail.com>
2024-02-22 20:51:36 +0530

    npm added in the repo.

---
+++ script.js
@@ -0,0 +1,11 @@
+const heading = React.createElement(
+    "h1",
+    {id: "heading"},
+    "Hi , I am thols !"
+)
+
+// console.log();
+const root = ReactDOM.createRoot(document.getElementsByClassName("root")[0]);
+
+
+root.render(heading);
