6084048fb8954410df0dfb0f7a52e599170aa324
tholkappiar <tholkappiar2003@gmail.com>
2024-02-29 22:36:07 +0530

    Added : search filter.

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,30 +3,58 @@ import Restaurent from "./Restaurent";
 
 const Body = () => {
 	const [filterData, setFilterData] = useState([]);
+	const [Data, setData] = useState([]);
+
+	const [searchText, setSearchText] = useState("");
 
 	useEffect(() => {
-		const fetchData = async () => {
-			const response = await fetch(
-				"https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.89960&lng=80.22090&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-			);
-			const data = await response.json();
-			// setFilterData(data.data.cards[0].card.card.imageGridCards.info);
-			setFilterData(
-				data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
-			);
-			console.log(filterData);
-		};
 		fetchData();
 	}, []);
+
+	const fetchData = async () => {
+		const response = await fetch(
+			"https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.89960&lng=80.22090&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+		);
+		const data = await response.json();
+		var temp =
+			data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants;
+		setFilterData(temp);
+		setData(temp);
+	};
+
+	function search() {
+		if (searchText.trim() == "") {
+			fetchData();
+		} else {
+			let filterSearch = Data.filter((item) =>
+				item.info.name
+					.toLowerCase()
+					.trim()
+					.includes(searchText.toLowerCase().trim())
+			);
+			setFilterData(filterSearch);
+		}
+	}
 
 	return (
 		<>
 			<div>
+				<input
+					value={searchText}
+					onChange={(e) => {
+						setSearchText(e.target.value);
+						search();
+					}}
+					onKeyDown={(e) => {
+						if (e.key === "Backspace") {
+							search();
+						}
+					}}
+				></input>
 				<button
 					className="filter-btn"
 					onClick={() => {
 						const filteredData = filterData.filter((item) => {
-							// console.log(item.info.avgRating);
 							return item.info.avgRating > 4;
 						});
 						setFilterData(filteredData);
